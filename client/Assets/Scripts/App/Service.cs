9697dfa6a2cf6fa7ebd108d98a39c7d9595dccf8
meamin9 <meamin_n@163.com>
2018-07-09 21:38:49 +0800

    client

---
+++ client/Assets/Scripts/App/Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Singleton<T> where T: class, new()
+{
+    static T _instance;
+    T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+            }
+            return _instance;
+        }
+    }
+    void Destroy()
+    {
+        _instance = null;
+    }
+}
+
+interface IService
+{
+    void Init();
+    void Start();
+    void Stop();
+}
+
+
+class ServiceMgr
+{
+    List<IService> _serviceList = new List<IService>();
+
+    void VisitService(Action<IService> func)
+    {
+        foreach (var s in _serviceList) {
+            func(s);
+        }
+    }
+
+    void RegService(IService s)
+    {
+        _serviceList.Add(s);
+    }
+
+}
