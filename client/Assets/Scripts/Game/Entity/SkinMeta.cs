3cf0c5f0eb5a3473c308cd088d2ca585f323267e
meamin9 <meamin_n@163.com>
2020-04-19 15:08:03 +0800

    update

---
+++ client/Assets/Scripts/Game/Entity/SkinMeta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Game {
+    public class SkinMeta {
+        public string rootBone;
+        public string[] bones;
+    }
+}
---
+++ client/Assets/Tools/Editor/GenSkinMeta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Newtonsoft.Json;
+using AM.Game;
+using UnityEditor;
+using System.IO;
+
+namespace Tools {
+    public class GenSkinMeta {
+
+        public void Reset() {
+            
+        }
+        public void GenerateSkinData() {
+            var path = "Assets/Resources/Appearence/";
+            var fbxs = Directory.GetFiles(path, "*_Preview.FBX", SearchOption.AllDirectories);
+            foreach(var fbx in fbxs) {
+                var asset = AssetDatabase.LoadAssetAtPath<GameObject>(fbx);
+            }
+            //var smr = GetComponent<SkinnedMeshRenderer>();
+            var bones = smr.bones;
+
+        }
+    }
+}
